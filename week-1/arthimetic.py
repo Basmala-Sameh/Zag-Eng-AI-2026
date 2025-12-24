404f8a97aa86133288c2a817971acee0e5aa311e
Basmala <basmala.2005sameh@gmail.com>
2025-12-24 09:24:16 +0200

    arthemitic operations on two numbers solution

---
+++ week-1/arthimetic.py
@@ -0,0 +1,24 @@
+# x, y = float(input("Enter the 2 numbers "))
+# float could not convert string to float 
+# x, y = float(input("Enter the 2 numbers ").split())
+# float take one number , couldn't take list
+
+x ,y = map(float,input("Enter the 2 numbers").split())
+sum = x+y
+subtract = x-y
+product = x*y
+
+big = (x,y)
+small = min(x,y)
+divission = None
+
+if small==0:
+    divission = "can't divde by zero"
+else:
+    divission = x/y
+
+
+print (f"Summition of your 2 numbers is : {sum}") 
+print (f"Subtraction of your 2 numbers is : {subtract}") 
+print (f"Division of your 2 numbers is : {divission}") 
+print (f"Product of your 2 numbers is : {product}") 
