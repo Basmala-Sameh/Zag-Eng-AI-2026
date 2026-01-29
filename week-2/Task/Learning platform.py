746400507497f21825c1627d3822ac8141727cd6
Basmala <basmala.2005sameh@gmail.com>
2026-01-29 04:15:21 +0200

    check_password() , set_password(), get_password()

--- week-2/Task/Learning platform.py
+++ week-2/Task/Learning platform.py
@@ -6,6 +6,25 @@ class User:
 
     def Show_info(self):
         return {
-            f"Name : {self.name}"
-            f"Email : {self.email}"
+            "Name" : self.name , 
+            "Email" : self.email
         }
+
+    def check_password(self , old_p ):
+        if self.__password == old_p :
+            return True
+
+    def set_password(self , old_p ,new_p ):
+        if self.check_password(old_p) == True :
+            self.__password = new_p
+
+    def get_password(self):
+        return self.__password
+
+
+
+class Student(User):
+    def __init__(self ,name , email ,password , level , specialization ):
+        super().__init__(self ,name , email ,password)
+        self.level = level
+        self.specialization = specialization
