260276f503d5b801b3704f6d65ed2479f0d4c09a
Basmala <basmala.2005sameh@gmail.com>
2026-01-30 09:48:57 +0200

    fixing access course title

--- week-2/Task-2/packages/School_system_package/main.py
+++ week-2/Task-2/packages/School_system_package/main.py
@@ -8,4 +8,4 @@ t1 = teachers(40 , "Dr.Kareem" , "IT")
 c1 = course(3 , "Computer vision", t1)
 s1.enroll(c1)
 
-print(f"{s1.name} enrolled in {s1.course}")
+print(f"{s1.name} enrolled in {s1.course.title}")
