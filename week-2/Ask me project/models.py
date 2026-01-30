91dae6deca6ea4adf3314ce522c309a417d59779
Basmala <basmala.2005sameh@gmail.com>
2026-01-31 01:27:08 +0200

    create User class , and import secret library for random id

--- week-2/Ask me project/models.py
+++ week-2/Ask me project/models.py
@@ -1,3 +1,10 @@
+import secrets
+
 class User():
-    def __init(self , name , id , password, email , allow_anonymous ):
+    def __init__(self , name , password, email , allow_anonymous ):
         self.name = name
+        id = secrets.token_urlsafe(8)
+        self.id = id
+        self.__password = password
+        self.email = email
+        self.allow_anonymous = allow_anonymous
