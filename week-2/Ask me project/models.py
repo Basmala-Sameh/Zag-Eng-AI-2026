35be49d3889176be7b5a660931bf0cd8f2e8a23e
Basmala <basmala.2005sameh@gmail.com>
2026-01-31 01:57:47 +0200

    update User class , and import random library , making display id and system id

--- week-2/Ask me project/models.py
+++ week-2/Ask me project/models.py
@@ -1,10 +1,18 @@
 import secrets
+import random
 
 class User():
+
     def __init__(self , name , password, email , allow_anonymous ):
         self.name = name
-        id = secrets.token_urlsafe(8)
-        self.id = id
+
+        display_id = random.randint(100 , 1000)
+        system_id = secrets.token_urlsafe(8)
+
+        self.system_id = system_id
+        self.display_id = display_id
+
         self.__password = password
         self.email = email
         self.allow_anonymous = allow_anonymous
+
