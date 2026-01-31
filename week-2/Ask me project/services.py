5f91a66e2ba1b80d900a5afd0b2a1057ee277dd7
Basmala <basmala.2005sameh@gmail.com>
2026-01-31 19:49:17 +0200

    intialize QuestionServices class

--- week-2/Ask me project/services.py
+++ week-2/Ask me project/services.py
@@ -3,18 +3,32 @@ from models import Question
 from models import Thread
 from models import Anonymous
 
+users_dict = {}
 
 class UserServices():
     counter = 0
 
-    def __init__(self):
-        self.users_dict = {}
-
     def add_user(self ,name , password, email , allow_anonymous):
         u = User(name , password, email , allow_anonymous)
-        self.users_dict[u.getId()] = u
+        users_dict[u.getId()] = u
         UserServices.counter +=1
 
 
+class QuestionServices():
+    def AskQuestion(self ,from_user_id , to_user_id , value):
+        Q = Question(from_user_id , to_user_id , value)
 
+        from_user_obj = users_dict[from_user_id]
+        to_user_obj = users_dict[to_user_id]
+
+        from_user_obj.from_me_q[Q.parent_id] = [Q.to_user_id ,value , Q.answered  ]
+
+        pass
+
+    def AQ():
+        pass
+
+    def thread():
+        pass
+    pass
 
